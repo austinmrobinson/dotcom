748f0c0a7238c96e2a3c17c25ad818b44c964cc0
Austin Robinson <austinrobinsondesign@gmail.com>
2024-04-02 21:39:56 -0500

    trying to fix?

--- middleware.ts
+++ middleware.ts
@@ -3,14 +3,19 @@ import type { NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
   const password = request.nextUrl.searchParams.get(
-    process.env.SEARCH_QUERY_NAME!
+    process.env.NEXT_PUBLIC_SEARCH_QUERY_NAME!
   );
-  const hasCookie = request.cookies.has(process.env.PASSWORD_COOKIE_NAME!);
+  const hasCookie = request.cookies.has(
+    process.env.NEXT_PUBLIC_PASSWORD_COOKIE_NAME!
+  );
   const url = request.nextUrl.clone();
   const response = NextResponse.redirect(url);
 
-  if (password === process.env.PAGE_PASSWORD && !hasCookie) {
-    response.cookies.set(`${process.env.PASSWORD_COOKIE_NAME}`, "true");
+  if (password === process.env.NEXT_PUBLIC_PAGE_PASSWORD && !hasCookie) {
+    response.cookies.set(
+      `${process.env.NEXT_PUBLIC_PASSWORD_COOKIE_NAME}`,
+      "true"
+    );
     return response;
   }
 }
