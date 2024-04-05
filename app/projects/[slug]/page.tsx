2438bbd396e7614be409fa6667cd7c19b6d646ef
Austin Robinson <austinrobinsondesign@gmail.com>
2024-04-05 08:48:20 -0500

    trying to see if we get slug

--- app/projects/[slug]/page.tsx
+++ app/projects/[slug]/page.tsx
@@ -17,74 +17,70 @@ export const metadata: Metadata = {
   title: "Projects",
 };
 
-interface ProjectPageProps {
-  params: {
-    slug: string;
-  };
+export default async function ProjectPage({ params }: any) {
+  // let projects = await newGetProjects();
+  // let project = projects?.find(
+  //   (project) => project.metadata.slug === params.slug
+  // );
+
+  // const cookiesStore = cookies();
+  // const loginCookies = cookiesStore.get(process.env.PASSWORD_COOKIE_NAME!);
+  // const isLoggedIn = !!loginCookies?.value;
+
+  return <div>{params.slug}</div>;
 }
-
-export default async function ProjectPage({ params }: ProjectPageProps) {
-  let projects = await newGetProjects();
-  let project = projects?.find(
-    (project) => project.metadata.slug === params.slug
-  );
-
-  const cookiesStore = cookies();
-  const loginCookies = cookiesStore.get(process.env.PASSWORD_COOKIE_NAME!);
-  const isLoggedIn = !!loginCookies?.value;
-
-  if (!isLoggedIn) {
-    return <PasswordForm />;
-  } else {
-    if (!project) return notFound();
-    return (
-      <Suspense
-        fallback={!isLoggedIn ? <PasswordForm /> : <ProjectPageLoading />}
-      >
-        <Animate className="flex flex-col gap-12">
-          <TopOfPage title={project.metadata.title} back="/projects">
-            <Text>{`${formatDateMonth(project.metadata.date)} • ${
-              project.metadata.company
-            } • ${project.metadata.role}`}</Text>
-          </TopOfPage>
-          <ImageZoom
-            src={project.metadata.thumbnail.src}
-            alt={project.metadata.thumbnail.alt}
-            className="w-full rounded-none md:rounded-xl bg-neutral-900/10 dark:bg-white/10"
-            buttonClassName="max-w-[767px] w-[100vw] self-center"
-          />
-          <PostBody>{project?.content}</PostBody>
-          {/* {project.metadata.images && (
-            <section className="flex flex-col gap-4">
-              <Heading size="h3" as="h2">
-                Gallery
-              </Heading>
-              <ImageZoomGallery images={project.metadata.images} />
-            </section>
-          )} */}
-          {/* {project.metadata.categories && (
-            <div className="flex flex-col gap-2">
-              <Heading size="h5" as="h2">
-                Categories
-              </Heading>
-              {project.metadata.categories && (
-                <ul className="flex gap-2">
-                  {project.metadata.categories.map(
-                    (category: string, index: number) => (
-                      <li
-                        className="flex items-center justify-center px-3 py-1 rounded-full bg-neutral-100 border border-neutral-900/5 dark:bg-neutral-100/10 dark:border-neutral-100/5"
-                        key={index}
-                      >
-                        {category}
-                      </li>
-                    )
-                  )}
-                </ul>
-              )}
-            </div>
-          )} */}
-        </Animate>
-      </Suspense>
-    );
-  }
-}
+//   if (!isLoggedIn) {
+//     return <PasswordForm />;
+//   } else {
+//     if (!project) return notFound();
+//     return (
+//       <Suspense
+//         fallback={!isLoggedIn ? <PasswordForm /> : <ProjectPageLoading />}
+//       >
+//         <Animate className="flex flex-col gap-12">
+//           <TopOfPage title={project.metadata.title} back="/projects">
+//             <Text>{`${formatDateMonth(project.metadata.date)} • ${
+//               project.metadata.company
+//             } • ${project.metadata.role}`}</Text>
+//           </TopOfPage>
+//           <ImageZoom
+//             src={project.metadata.thumbnail.src}
+//             alt={project.metadata.thumbnail.alt}
+//             className="w-full rounded-none md:rounded-xl bg-neutral-900/10 dark:bg-white/10"
+//             buttonClassName="max-w-[767px] w-[100vw] self-center"
+//           />
+//           <PostBody>{project?.content}</PostBody>
+//           {/* {project.metadata.images && (
+//             <section className="flex flex-col gap-4">
+//               <Heading size="h3" as="h2">
+//                 Gallery
+//               </Heading>
+//               <ImageZoomGallery images={project.metadata.images} />
+//             </section>
+//           )} */}
+//           {/* {project.metadata.categories && (
+//             <div className="flex flex-col gap-2">
+//               <Heading size="h5" as="h2">
+//                 Categories
+//               </Heading>
+//               {project.metadata.categories && (
+//                 <ul className="flex gap-2">
+//                   {project.metadata.categories.map(
+//                     (category: string, index: number) => (
+//                       <li
+//                         className="flex items-center justify-center px-3 py-1 rounded-full bg-neutral-100 border border-neutral-900/5 dark:bg-neutral-100/10 dark:border-neutral-100/5"
+//                         key={index}
+//                       >
+//                         {category}
+//                       </li>
+//                     )
+//                   )}
+//                 </ul>
+//               )}
+//             </div>
+//           )} */}
+//         </Animate>
+//       </Suspense>
+//     );
+//   }
+// }
